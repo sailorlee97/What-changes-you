86d0ea4459d41100f29ae63f3d6f82a13490c522
Zeyi <50068428+sailorlee97@users.noreply.github.com>
2023-11-21 11:15:15 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,36 +1,59 @@
-import os
-import os.path
-import torchvision
-import torch
-from torch import nn
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-from torch.utils.data.dataloader import default_collate
+import threading
+import psutil
+import GPUtil
+import time
 
-def showGod():
-    print("""
-                                _ooOoo_
-                               o8888888o
-                               88" . "88
-                               (| -_- |)
-                               O\  =  /O
-                            ____/`---'\____
-                          .'  \\|     |//  `.
-                         /  \\|||  :  |||//  \\
-                        /  _||||| -:- |||||- \\
-                        |   | \\\  -  /// |   |
-                        | \_|  ''\---/''  |   |
-                        \  .-\__  `-`  ___/-. /
-                      ___`. .'  /--.--\  `. . __
-                   ."" '<  `.___\_<|>_/___.'  >'"".
-                  | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-                  \  \ `-.   \_ __\ /__ _/   .-` /  /
-             ======`-.____`-.___\_____/___.-`____.-'======
-                                `=---='
-            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-                        GOD BLESS ME, NEVER GET BUG
-                         1. check the image size
-                         2. optimizer zero grad
-                         3. test/eval model.eval()
-                         4. pretrained resnet is for imagenet,kernel=7
-    """)
+class CpuGpuMonitor:
+    def __init__(self):
+        self.cpu_data = []
+        self.gpu_data = []
+        self.flag = threading.Event()
+        self.monitor_thread = None
+
+    def start(self):
+        self.flag.set()
+        self.monitor_thread = threading.Thread(target=self.update_usage)
+        self.monitor_thread.start()
+
+    def end(self):
+        # Set the flag to stop the monitoring thread
+        self.flag.clear()
+        # Wait for the monitoring thread to finish
+        self.monitor_thread.join()
+        # Print the average CPU and GPU usage
+        print(f"Average CPU usage: {self.get_cpu_average_usage()}%")
+        print(f"Average GPU usage: {self.get_gpu_average_usage()}%")
+
+    def update_usage(self):
+        while self.flag.is_set():
+            time.sleep(1)
+            cpu_percent = psutil.cpu_percent(interval=1)
+            self.cpu_data.append(cpu_percent)
+            gpu_percent = GPUtil.getGPUs()[0].load * 100
+            self.gpu_data.append(gpu_percent)
+
+    def get_cpu_average_usage(self):
+        return sum(self.cpu_data) / len(self.cpu_data) if len(self.cpu_data) > 0 else 0
+
+    def get_gpu_average_usage(self):
+        return sum(self.gpu_data) / len(self.gpu_data) if len(self.gpu_data) > 0 else 0
+
+if __name__ == '__main__':
+
+    # 创建监控对象
+    monitor = CpuGpuMonitor()
+
+    # 开始监控
+    monitor.start()
+
+    # 持续更新CPU和GPU使用率数据
+    while True:
+        # 更新CPU和GPU使用率数据
+        monitor.update_usage()
+
+    # 结束监控
+    monitor.end()
+
+    # 输出CPU和GPU平均使用率
+    print(f"CPU average usage: {monitor.get_cpu_average_usage():.2f}%")
+    print(f"GPU average usage: {monitor.get_gpu_average_usage():.2f}%")
